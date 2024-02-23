8f96606bab9af05d79e176c46517c3246566ecc4
sufyan4081 <srasheed4081@gmail.com>
2024-02-23 15:27:28 +0530

    fix the updating api

--- controllers/dishController.js
+++ controllers/dishController.js
@@ -73,29 +73,54 @@ const fetchAllDish = async (req, res) => {
 
 const updateDish = async (req, res) => {
   try {
-    const Item = await Dish.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      useFindAndModify: true,
-      runValidators: true,
+    let form = new multiparty.Form();
+
+    form.parse(req, async function (err, fields, files) {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+
+      const updateFields = {
+        title: fields.title ? fields.title[0] : undefined,
+        name: fields.name ? fields.name[0] : undefined,
+        description: fields.description ? fields.description[0] : undefined,
+        price: fields.price ? fields.price[0] : undefined,
+        rating: fields.rating ? fields.rating[0] : undefined,
+      };
+
+      if (files.image) {
+        // Assuming the image is uploaded and stored in the 'image' field
+        const imageFile = files.image[0];
+
+        // Read the image file and convert it to base64
+        const imageBuffer = fs.readFileSync(imageFile.path);
+        const base64Image = imageBuffer.toString("base64");
+
+        // Construct the base64 image URL
+        updateFields.image = `data:${imageFile.headers["content-type"]};base64,${base64Image}`;
+      }
+
+      const Item = await Dish.findByIdAndUpdate(req.params.id, updateFields, {
+        new: true,
+        useFindAndModify: true,
+        runValidators: true,
+      });
+
+      if (Item) {
+        res.status(200).json({
+          success: true,
+          Item,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "Dish not found",
+        });
+      }
     });
-    if (Item) {
-      res.status(200).json({
-        success: true,
-        Item,
-      });
-    } else {
-      res.status(404).json({
-        success: false,
-        message: "Dish not found",
-      });
-    }
   } catch (error) {
-    if (error) {
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
-    }
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
